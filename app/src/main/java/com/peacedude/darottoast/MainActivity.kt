483893dfdc32225e193d09ed8dbfa07373d08314
darot <darotudeen@gmail.com>
2020-06-26 17:12:02 +0100

    prepared for first release

--- app/src/main/java/com/peacedude/darottoast/MainActivity.kt
+++ app/src/main/java/com/peacedude/darottoast/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.peacedude.gdtoast.gdErrorToast
 import com.peacedude.gdtoast.gdToast
 
 
@@ -13,16 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gdToast(
-            message = "Hello World",
-            icon = R.drawable.ic_android_green_24dp,
-            backgroundColor = R.color.newColor,
-            textColor = R.color.colorPrimaryDark,
-            gravity = Gravity.TOP,
-            textGravity = Gravity.START,
-            x_Offset = 10,
-            y_OffSet = 20,
-            toastDuration = Toast.LENGTH_SHORT
-        )
+        gdErrorToast("Error World", Gravity.TOP)
     }
 }
