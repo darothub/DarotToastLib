257651200a8e432aa983f1a1126d53ec0977c1da
darot <darotudeen@gmail.com>
2020-06-26 18:38:19 +0100

    update documentation

--- app/src/main/java/com/peacedude/darottoast/MainActivity.kt
+++ app/src/main/java/com/peacedude/darottoast/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.peacedude.gdtoast.Darot
 import com.peacedude.gdtoast.gdErrorToast
 import com.peacedude.gdtoast.gdToast
 
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Darot.getGdDrawableBackgrnd = R.drawable.gd_toast_bkgrnd
 
         gdErrorToast("Error World", Gravity.TOP)
     }
