cd80db6615d678f230858938b76d184173dc90d3
darot <darotudeen@gmail.com>
2020-06-26 10:35:01 +0100

    Added documentation in read me

--- app/src/main/java/com/peacedude/darottoast/MainActivity.kt
+++ app/src/main/java/com/peacedude/darottoast/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.peacedude.darottoast
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import com.peacedude.gdtoast.gdToast
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gdToast("Hello World!", Gravity.CENTER)
     }
 }
