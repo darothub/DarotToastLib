4ada6f1399677d8552cae58982b30e817a3ff45b
Idris Aminu <Idris@ptml.lan>
2020-10-20 11:53:13 +0100

    Implementation of custom duration of toast display with already pre-existing Toast.SHORT and Toast.LONG to support longer duration

--- app/src/main/java/com/peacedude/darottoast/MainActivity.kt
+++ app/src/main/java/com/peacedude/darottoast/MainActivity.kt
@@ -2,7 +2,9 @@ package com.peacedude.darottoast
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.peacedude.gdtoast.Darot
@@ -18,9 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         Darot.getGdDrawableBackgrnd = R.drawable.gd_toast_bkgrnd
         val drawab = ContextCompat.getDrawable(this, R.drawable.ic_android_green_24dp)
+//        gdToast(
+//            "Error World Error WorldError WorldError WorldError World WorldError World WorldError World",
+//            icon= drawab
+//        )
+
+    }
+
+    fun click(view: View){
         gdToast(
-            "Error World Error WorldError WorldError WorldError World WorldError World WorldError World",
-            icon= drawab
+            message =  "Testing toast here",
+            icon = R.drawable.ic_android_green_24dp,
+            backgroundColor = R.color.colorAccent,
+            textColor = R.color.colorBlueDark,
+            gravity = Gravity.TOP,
+            textGravity = 20,
+            x_Offset = 10,
+            y_OffSet = 10,
+            toastDuration = Toast.LENGTH_LONG
         )
     }
 }
